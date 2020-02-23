a42946c6269d64cdfc3cada7d0d107be8854c174
bassyess <gaoxk2017@163.com>
2020-02-23 17:30:50 +0800

    20200223

--- 20200223/leetcode148.py
+++ 20200223/leetcode148.py
@@ -30,4 +30,33 @@ class Solution:
         right = self.sortList(mid)
         return merge(left, right)
 
-# 快排实现
+# 快排实现（超出时间限制？）
+class Solution2:
+    def sortList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+
+        def partition(begin, end):
+            pivot = p1 = begin
+            p2 = p1.next
+            while p2 != end:
+                if p2.val < pivot.val:
+                    p1 = p1.next
+                    tmp = p2.val
+                    p2.val = p1.val
+                    p1.val = tmp
+                p2 = p2.next
+            tmp = pivot.val
+            pivot.val = p1.val
+            p1.val = tmp
+            return p1
+
+        def quickSort(begin, end):
+            if begin == end:
+                return
+            pNode = partition(begin, end)
+            quickSort(begin, pNode)
+            quickSort(pNode.next, end)
+
+        quickSort(head, None)
+        return head
--- 20200223/leetcode912.py
+++ 20200223/leetcode912.py
@@ -25,34 +25,3 @@ class Solution:
             nums[0], nums[i] = nums[i], nums[0]
             adjustHeap(nums, 0, i)
         return nums
-
-# 快排实现（超出时间限制？）
-class Solution2:
-    def sortList(self, head: ListNode) -> ListNode:
-        if not head or not head.next:
-            return head
-
-        def partition(begin, end):
-            pivot = p1 = begin
-            p2 = p1.next
-            while p2 != end:
-                if p2.val < pivot.val:
-                    p1 = p1.next
-                    tmp = p2.val
-                    p2.val = p1.val
-                    p1.val = tmp
-                p2 = p2.next
-            tmp = pivot.val
-            pivot.val = p1.val
-            p1.val = tmp
-            return p1
-
-        def quickSort(begin, end):
-            if begin == end:
-                return
-            pNode = partition(begin, end)
-            quickSort(begin, pNode)
-            quickSort(pNode.next, end)
-
-        quickSort(head, None)
-        return head
